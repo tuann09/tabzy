5d01c18e562afe948b77554e4e006a35d90ae894
Tuans09 <dt2003911@gmail.com>
2025-03-10 18:19:23 +0700

    tao phuong thuc switch va destroy

--- src/tabzy.js
+++ src/tabzy.js
@@ -25,20 +25,21 @@ function Tabzy(selector) {
         })
         .filter(Boolean);
     if (this.tabs.length !== this.panels.length) return;
+    this._originalHTML = this.container.innerHTML;
     this._init();
 }
 Tabzy.prototype._init = function () {
-    const tabActive = this.tabs[0];
-    tabActive.closest("li").classList.add("tabzy--active");
-    this.panels.forEach((panel) => (panel.hidden = true));
+    this._activeTab(this.tabs[0]);
+
     this.tabs.forEach((tab) => {
         tab.onclick = (event) => this._handleTabClick(event, tab);
     });
-    const panelActive = this.panels[0];
-    panelActive.hidden = false;
 };
 Tabzy.prototype._handleTabClick = function (event, tab) {
     event.preventDefault();
+    this._activeTab(tab);
+};
+Tabzy.prototype._activeTab = function (tab) {
     this.tabs.forEach((tab) => {
         tab.closest("li").classList.remove("tabzy--active");
     });
@@ -46,4 +47,31 @@ Tabzy.prototype._handleTabClick = function (event, tab) {
     this.panels.forEach((panel) => (panel.hidden = true));
     const panelActive = document.querySelector(tab.getAttribute("href"));
     panelActive.hidden = false;
+};
+Tabzy.prototype.switch = function (input) {
+    let tabToActive = null;
+
+    if (typeof input === "string") {
+        tabToActive = this.tabs.find(
+            (tab) => tab.getAttribute("href") === input
+        );
+        if (!tabToActive) {
+            console.error(`Tabzy: No panel found with ID '${input}'`);
+            return;
+        }
+    } else if (this.tabs.includes(input)) {
+        tabToActive = input;
+    }
+    if (!tabToActive) {
+        console.error(`Tabzy: Invalid input '${input}'`);
+        return;
+    }
+    this._activeTab(tabToActive);
+};
+Tabzy.prototype.destroy = function () {
+    this.container.innerHTML = this._originalHTML;
+    this.panels.forEach((panel) => (panel.hidden = false));
+    this.container = null;
+    this.tabs = null;
+    this.panels = null;
 };
